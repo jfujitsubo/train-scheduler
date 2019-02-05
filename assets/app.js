22ab8b7f6b0f7ff0e8bbcce2129cbc8416358b81
Fooji <jfujitsubo@yahoo.com>
2019-02-04 23:17:12 -0800

    couldnt figure out how to initialize firebase properly

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,30 @@
-$(document).ready(function(){
-	
-	var trainBase = new Firebase("//add fireBase Link//");
 
-	$("#addButton").on("click", function(){
+
+
+$(document).ready(function(){
+/*couldnt get firebase to initialize properly*/    
+    var config = {
+        apiKey: "AIzaSyAK9cJZsO-O0byJFhsPO9OFQwYC3qmPx0M",
+        authDomain: "ucla-example-8435d.firebaseapp.com",
+        databaseURL: "https://ucla-example-8435d.firebaseio.com",
+        projectId: "ucla-example-8435d",
+        storageBucket: "ucla-example-8435d.appspot.com",
+        messagingSenderId: "279686285438"
+      };
+      firebase.initializeApp(config);
+
+	var trainDatabase = firebase.database();
+
+/*	tried to create variable to hold the firebase data, and input values that way
+couldnt get firebase to initialize properly. Probably should have figure that out first
+
+attempted to use firebase with var variableName = new Firebase("firebase link here").
+that didnt work. so i followed the instructions on the website and that didnt work either.
+i missed the second half of the lecture for this session due to me leaving early to help out my mom
+as i do on weekdays. however, i couldnt find the entire lecture online, and prioritized
+Project #1 over getting help on this -___-. Will figure out and push changes when solved.
+
+    $("#addButton").on("click", function(){
 
 		var trainName = $("#trainNameVal").val().trim();
 		var lineName = $("#lineVal").val().trim();
@@ -19,7 +41,7 @@ $(document).ready(function(){
 			frequency: frequencyVal,
 		}
 
-        trainBase.push(newTrain);
+        trainDatabase.push(newTrain);
 
 		
 		$("#trainNameVal").val("");
@@ -31,9 +53,7 @@ $(document).ready(function(){
     });
     
     //receiving data from database	
-    trainBase.on("child_added", function(childSnapshot, previousChild){
-
-    console.log(childSnapshot.val());
+    trainDatabase.on("child_added", function(childSnapshot, previousChild){
 
     //making vars for snapshots
     var firebaseName = childSnapshot.val().name;
@@ -50,7 +70,7 @@ $(document).ready(function(){
 		$("#trainDiv > tbody").append("<tr><td>" + firebaseName + "</td><td>" + firebaseLine + "</td><td>"+ firebaseDestination + "</td><td>" + firebaseFrequency + " mins" + "</td><td>" + nextTrainArrival + "</td><td>" + minutes + "</td></tr>");
 
 	});
-
+*/
     
     
 });
